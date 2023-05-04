29399470d8f5e1b38cc2257768ad93fcb3dbeb4e
Zsolt Ringhofer <sztego@gmail.com>
2023-05-04 10:57:22 +0200

    new features

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,10 +7,11 @@ using namespace std;
 int main(){
 
     vector<string> msg {"Hello","C++","World","from","VS Code","and the c++ extension!"};
+    msg.push_back("Wow!!");
     int i = 0;
     for (const string& word : msg)
     {
-        cout << word << " ";
+        cout << word << "!! ";
         i++;
     }
     cout << endl;
